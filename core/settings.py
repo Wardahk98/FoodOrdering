685a22eb428078ba02f7603b2b87b4f10fffdabc
Wardahk98 <wardahk98@gmail.com>
2024-08-08 15:14:14 +0500

    Add accounts and adjust frontend

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
 
 EXTERNAL_APPS = [
     'products',
+    'home',
+    'account',
 ]
 
 INSTALLED_APPS += EXTERNAL_APPS
@@ -116,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import *
+from home.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('about/', about, name="about"),
+    path('contact/', contact, name="contact")
+
 ]
